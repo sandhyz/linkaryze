dabdb82eba5767440dc7ab9e9731a2fde842b3b3
sandhy <sandhyzidan@gmail.com>
2023-01-17 10:30:55 +0700

    setup

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import topBar from './layout/topbar/TopBar.vue'
+</script>
+
+<template>
+  <h1>main</h1>
+</template>
+
+<style scoped>
+
+</style>
